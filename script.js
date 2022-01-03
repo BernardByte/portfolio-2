21c3084ac52db2537f4cd334c34f0f3b912cada2
pakistanofficial <karachilahore117@gmail.com>
2022-01-03 10:19:45 +0500

    fgt

--- script.js
+++ script.js
@@ -1,43 +1,73 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-    // Button cancel and show 
-let menuBtn = document.querySelector(".menu-btn");
-let cancelBtn = document.querySelector(".cancel-btn");
-let navBar = document.querySelector(".navbar");
-let body = document.querySelector("body");
-menuBtn.onclick = function(){
-    menuBtn.style.opacity = "0";
-    menuBtn.style.pointerEvents = "none";
-    navBar.classList.add("active");
-    body.style.overflow = "hidden";
-}
-cancelBtn.onclick = function(){
-    menuBtn.style.opacity = "1";
-    menuBtn.style.pointerEvents = "auto";
-    navBar.classList.remove("active");
-    body.style.overflow = "auto";
-}
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
 
-// navbar show with other color
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
-let nav = document.querySelector("nav");
- let val;
- window.onscroll = function() {
-     if(document.documentElement.scrollTop >20){
-         nav.classList.add("sticky");
-     }
-     else {
-         nav.classList.remove("sticky");
-     }
- }
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 
-// side navigation menu closed when navigation links are clicked
- let navLinks = document.querySelectorAll(".menu li a");
- for (let i = 0; i < navLinks.length; i++) {
-      navLinks[i].addEventListener("click", ()=>{
-        menuBtn.style.opacity = "1";
-        menuBtn.style.pointerEvents = "auto";
-        navBar.classList.remove("active");
-        body.style.overflow = "auto";
-      });
-     
- }
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
